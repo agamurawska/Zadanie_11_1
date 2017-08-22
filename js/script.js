7eb21b16f5d6c7fc54c10f790a31caf31835499c
aga <aga.murawska@gmail.com>
2017-08-22 22:57:44 +0200

    phones excercise

---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿function Phone (brand, price, color){
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+};
+Phone.prototype.printInfo = function(){
+    console.log("the phone brand is " + this.brand + ", color is " + this.color + ",and the price is... " + this.price + ".");
+};
+var Samsung = new Phone("Samsung Galaxy S6", "well, you still can't afford it", "deep blue"),
+    iPhone = new Phone("iPhone6s","cheap","white"),
+    OnePlus = new Phone("OnePlus One", "1000$","red");
+
+Samsung.printInfo();
